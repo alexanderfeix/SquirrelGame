8f28f7e56f0d69a53a30136a1cd82580fd46a230
Felix Herbst <felix030503@gmail.com>
2022-03-28 20:32:56 +0200

    adding shit

--- src/main/java/hs/augsburg/squirrelgame/entity/squirrel/HandOperatedMasterSquirrel.java
+++ src/main/java/hs/augsburg/squirrelgame/entity/squirrel/HandOperatedMasterSquirrel.java
@@ -1,4 +1,21 @@
 package hs.augsburg.squirrelgame.entity.squirrel;
 
-public class HandOperatedMasterSquirrel {
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+
+public class HandOperatedMasterSquirrel implements KeyListener {
+    @Override
+    public void keyTyped(KeyEvent e) {
+    System.out.println("You typed:" + e.getKeyChar());
+    }
+
+    @Override
+    public void keyPressed(KeyEvent e) {
+
+    }
+
+    @Override
+    public void keyReleased(KeyEvent e) {
+
+    }
 }
