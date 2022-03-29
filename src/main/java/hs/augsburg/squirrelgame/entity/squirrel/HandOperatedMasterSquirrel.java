6ddac96b7cf98753704c5ac079df5b4c349ef9dd
Felix Herbst <felix030503@gmail.com>
2022-03-29 10:49:43 +0200

    adding gui and enum and keylistener and shit

--- src/main/java/hs/augsburg/squirrelgame/entity/squirrel/HandOperatedMasterSquirrel.java
+++ src/main/java/hs/augsburg/squirrelgame/entity/squirrel/HandOperatedMasterSquirrel.java
@@ -4,18 +4,27 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class HandOperatedMasterSquirrel implements KeyListener {
+
+
     @Override
     public void keyTyped(KeyEvent e) {
-    System.out.println("You typed:" + e.getKeyChar());
+        if(e.getKeyChar() == 'w' ){
+            System.out.println("up");
+        }else if(e.getKeyChar() == 'a' ){
+            System.out.println("left");
+        }else if(e.getKeyChar() == 's' ){
+            System.out.println("down");
+        } else if (e.getKeyChar() == 'd' ) {
+            System.out.println("right");
+        }
     }
 
     @Override
     public void keyPressed(KeyEvent e) {
-
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
-
     }
 }
+
