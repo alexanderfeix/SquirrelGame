8248ad37e47bf9bbdfe9744312526b02f5d2158a
Alexander F <taventix@gmail.com>
2022-06-08 22:58:36 +0200

    added comments to algorithm

--- src/main/java/hs/augsburg/squirrelgame/botimpls/algorithm/AStarAlgorithmHandler.java
+++ src/main/java/hs/augsburg/squirrelgame/botimpls/algorithm/AStarAlgorithmHandler.java
@@ -8,18 +8,24 @@ import java.util.List;
 
 public class AStarAlgorithmHandler {
 
+    /**
+     * This is currently just a test handler class for the A* algorithm.
+     * More information about A* algorithm: https://www.youtube.com/watch?v=-L-WgKMFuhE&t=618s
+     */
+
 
     //The list of nodes to be evaluated
     private static List<AStarNode> openNodes = new ArrayList<>();
     //The list of nodes already evaluated
     private static List<AStarNode> closedNodes = new ArrayList<>();
 
-    public static AStarNode[][] gameBoard = new AStarNode[20][20];
+    public static AStarNode[][] gameBoard = new AStarNode[100][100];
 
 
     public static void main(String[] args) {
+        //You can change the XY positions for sure :)
         XY start = new XY(10, 10);
-        XY target = new XY(15, 10);
+        XY target = new XY(15, 12);
         createObstacle();
         evaluate(start, target);
     }
@@ -29,36 +35,20 @@ public class AStarAlgorithmHandler {
         openNodes.add(startNode);
         int counter = 0;
         while(true){
-            Iterator<AStarNode> openNodesIterator = openNodes.iterator();
-            while(openNodesIterator.hasNext()){
-                System.out.println("OpenNode: " + openNodesIterator.next().getPosition().toString());
-            }
-            Iterator<AStarNode> closedNodesIterator = closedNodes.iterator();
-            while(closedNodesIterator.hasNext()){
-                System.out.println("ClosedNode: " + closedNodesIterator.next().getPosition().toString());
-            }
-            System.out.println("Loop count: " + counter++);
-
-
-
+            counter++;
             AStarNode currentNode = getNodeWithLowestFCost();
             openNodes.remove(currentNode);
             closedNodes.add(currentNode);
             if(currentNode.getPosition().toString().equalsIgnoreCase(targetPosition.toString())){
                 //Path has been found
-                System.out.println("Path found!");
-                for(AStarNode aStarNode : closedNodes){
-                    System.out.println("Node in closed list: " + aStarNode.getPosition().toString());
-                }
+                System.out.println("Path found! Took " + counter + " runs.\nPath-List: ");
                 printParents(currentNode);
                 return;
             }
             for(int col = currentNode.getPosition().getX() - 1; col <= currentNode.getPosition().getX() + 1; col++){
                 for(int row = currentNode.getPosition().getY() - 1; row <= currentNode.getPosition().getY() + 1; row++){
                     AStarNode neighbour = new AStarNode(currentNode, new XY(col, row), startNode.getPosition(), targetPosition);
-                    System.out.println(neighbour.getPosition().toString());
                     if(gameBoard[col][row] != null || containsNode(closedNodes, neighbour)) {
-                        System.out.println("Continued!");
                         continue;
                     }
                     if(!containsNode(openNodes, neighbour) || isPathShorter(neighbour)){
@@ -110,6 +100,9 @@ public class AStarAlgorithmHandler {
         return null;
     }
 
+    /**
+     * You can put some Obstacles on the board here to make the pathfinding a little bit more interesting :)
+     */
     private static void createObstacle(){
         XY xy = new XY(1, 1);
         gameBoard[14][9] = new AStarNode(null, xy, xy, xy);
